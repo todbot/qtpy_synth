dbad1105cca42456a51225508f7348a5bb0c5cec
Tod Kurt <todbot@users.noreply.github.com>
2023-08-04 18:41:21 -0700

    add midi to simpletouchsynth because why not

--- circuitpython/simpletouchsynth/code.py
+++ circuitpython/simpletouchsynth/code.py
@@ -2,15 +2,24 @@
 # 12 Jul 2023 - @todbot / Tod Kurt
 # part of https://github.com/todbot/qtpy_synth
 #
+# Needed libraries to install:
+#  circup install asyncio neopixel adafruit_debouncer adafruit_displayio_ssd1306 adafruit_display_text adafruit_midi
+#
 
+import asyncio
 import time
 import random
-import asyncio
 import synthio
 import ulab.numpy as np
 
 import displayio, terminalio, vectorio
 from adafruit_display_text import bitmap_label as label
+
+import usb_midi
+import adafruit_midi
+from adafruit_midi.note_on import NoteOn
+from adafruit_midi.note_off import NoteOff
+from adafruit_midi.control_change import ControlChange
 
 from qtpy_synth import QTPySynth
 
@@ -19,16 +28,20 @@ class SynthConfig():
         self.filter_type = 'lpf'
         self.filter_f = 2000
         self.filter_q = 1.2
+        self.filter_mod = 0
 
 qts = QTPySynth()
 cfg = SynthConfig()
-f_orig = 0
 
 touch_midi_notes = [40, 48, 52, 60] # can be float
-touch_notes = [None] * 4
+notes_playing = {}  # dict of notes currently playing
+
+# let's get the midi going
+midi_usb = adafruit_midi.MIDI(midi_in=usb_midi.ports[0], in_channel=0 )
+midi_uart = adafruit_midi.MIDI(midi_in=qts.midi_uart, in_channel=0 )
 
 # set up some default synth parameters
-wave_saw = np.linspace(30000,-30000, num=256, dtype=np.int16)  # default squ is too clippy, should be 3dB down or so
+wave_saw = np.linspace(30000,-30000, num=512, dtype=np.int16)  # default squ is too clippy, should be 3dB down or so
 amp_env = synthio.Envelope(sustain_level=0.8, release_time=0.6, attack_time=0.001)
 qts.synth.envelope = amp_env
 
@@ -36,7 +49,7 @@ qts.synth.envelope = amp_env
 disp_group = displayio.Group()
 qts.display.root_group = disp_group
 
-labels_pos = ( (5,5), (50,5), (80,5) )  #  filter_f, filter_type, filter_q
+labels_pos = ( (5,5), (50,5), (100,5) )  #  filter_f, filter_type, filter_q
 disp_info = displayio.Group()
 for (x,y) in labels_pos:
     disp_info.append( label.Label(terminalio.FONT, text="-", x=x, y=y) )
@@ -46,52 +59,59 @@ disp_group.append(disp_info)
 def map_range(s, a1, a2, b1, b2):  return  b1 + ((s - a1) * (b2 - b1) / (a2 - a1))
 
 def display_update():
-    f_str = "%4d" % cfg.filter_f
+    f_str = "%4d" % (cfg.filter_f + cfg.filter_mod)
     q_str = "%1.1f" % cfg.filter_q
 
     if f_str != disp_info[0].text:
-        disp_info[1].text = f_str
+        disp_info[0].text = f_str
 
     if cfg.filter_type != disp_info[1].text:
-        disp_info[0].text = cfg.filter_type
+        disp_info[1].text = cfg.filter_type
 
     if q_str != disp_info[2].text:
         disp_info[2].text = q_str
         print("edit q", q_str)
 
-def touch_on(i):  # callback
-    global f_orig
-    print("touch press",i)
-    f = synthio.midi_to_hz(touch_midi_notes[i])
-    n = synthio.Note( frequency=f, waveform=wave_saw, filter=make_filter() )
-    qts.synth.press( n )
-    touch_notes[i] = n
+def note_on( notenum, vel=64):
+    print("note_on", notenum, vel)
+    cfg.filter_mod = (vel/127) * 1500
+    f = synthio.midi_to_hz(notenum)
+    note = synthio.Note( frequency=f, waveform=wave_saw, filter=make_filter() )
+    notes_playing[notenum] = note
+    qts.synth.press( note )
     qts.led.fill(0xff00ff)
-    f_orig = cfg.filter_f
+    #f_orig = cfg.filter_f
+
+def note_off( notenum, vel=0):
+    #global f_orig
+    print("note_off", notenum, vel)
+    if note := notes_playing[notenum]:
+        qts.synth.release( note )
+    qts.led.fill(0)
+    #cfg.filter_f = f_orig
+    #f_orig = 0
+
+def touch_on(i):  # callback
+    note_on( touch_midi_notes[i] )
 
 def touch_off(i):  # callback
-    global f_orig
-    #print("touch release", i)
-    if touch_notes[i]:
-        qts.synth.release( touch_notes[i] )
-    qts.led.fill(0)
-    cfg.filter_f = f_orig
-    f_orig=0
+    note_off( touch_midi_notes[i] )
 
 def touch_hold(i,v):  # callback
-    vn = min(max(0, v),2000)
-    cfg.filter_f  = min(f_orig * (1 + vn/1000), 8000)
-    print("hold %d %d %d" % (i,vn, cfg.filter_f))
+    vn = min(max(0, v), 2000)  # ensure touch info stays positive
+    cfg.filter_mod  =  (vn/2000) * 3000   # range 0-3000
+    print("hold %d %d %d" % (i,vn, cfg.filter_mod))
 
 filter_types = ['lpf', 'hpf', 'bpf']
 
 def make_filter():
+    freq = cfg.filter_f + cfg.filter_mod
     if cfg.filter_type == 'lpf':
-        filter = qts.synth.low_pass_filter(cfg.filter_f, cfg.filter_q)
+        filter = qts.synth.low_pass_filter(freq, cfg.filter_q)
     elif cfg.filter_type == 'hpf':
-        filter = qts.synth.high_pass_filter(cfg.filter_f, cfg.filter_q)
+        filter = qts.synth.high_pass_filter(freq, cfg.filter_q)
     elif cfg.filter_type == 'bpf':
-        filter = qts.synth.band_pass_filter(cfg.filter_f, cfg.filter_q)
+        filter = qts.synth.band_pass_filter(freq, cfg.filter_q)
     else:
         print("unknown filter type", cfg.filter_type)
     return filter
@@ -112,33 +132,43 @@ async def input_handler():
     while True:
         key = qts.check_key()
         qts.check_touch( touch_on, touch_off, touch_hold )  # don't really like these callbacks
-
         (knobA, knobB) = qts.read_pots()
+
         if key == "pressed":
             ftpos = (filter_types.index(cfg.filter_type)+1) % len(filter_types)
             cfg.filter_type = filter_types[ ftpos ]
 
-        if f_orig==0:  # HACK
-            cfg.filter_f = map_range( knobA, 0,65535, 30, 4000)
-        cfg.filter_q = map_range( knobB, 0,65535, 0.1, 2.8)
+        #if f_orig==0:  # HACK
+        cfg.filter_f = map_range( knobA, 0,65535, 30, 8000)
+        cfg.filter_q = map_range( knobB, 0,65535, 0.1, 3.0)
 
         await asyncio.sleep(0.01)
 
 async def synth_updater():
     # for any notes playing, adjust its filter in realtime
     while True:
-        for n in touch_notes:
+        for n in notes_playing.values():
             if n:
                 n.filter = make_filter()
         await asyncio.sleep(0.01)
 
-print("qtpy_synth hwtest4 ready")
+async def midi_handler():
+    while True:
+        while msg := midi_usb.receive() or midi_uart.receive():
+            if isinstance(msg, NoteOn) and msg.velocity != 0:
+                note_on(msg.note, msg.velocity)
+            elif isinstance(msg,NoteOff) or isinstance(msg,NoteOn) and msg.velocity==0:
+                note_off(msg.note, msg.velocity)
+        await asyncio.sleep(0.001)
+
+print("-- qtpy_synth simpletouchsynth ready --")
 
 async def main():
     task1 = asyncio.create_task(display_updater())
     task2 = asyncio.create_task(input_handler())
     task3 = asyncio.create_task(synth_updater())
-    await asyncio.gather(task1,task2,task3)
+    task4 = asyncio.create_task(midi_handler())
+    await asyncio.gather(task1,task2,task3,task4)
 
 asyncio.run(main())
 
